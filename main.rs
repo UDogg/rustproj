55ba0316c7caed241bfcc79e4ea8868e66bfe81a
Utkarsh Choudhary <ufc5009@psu.edu>
2023-07-22 19:09:59 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,3 +1,12 @@
+struct MyStruct;
+
+impl MyStruct {
+    fn my_method() {
+        println!("This is a method of MyStruct.");
+    }
+}
+
 fn main() {
+    MyStruct::my_method();
     println!("Hello, World!");
 }
